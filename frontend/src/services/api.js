4faf3061033c7e55226eb5e6d4d13a80f58137e4
Gaurav Shevkar <gauravshevkar06@gmail.com>
2026-06-07 04:00:58 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://tdc-matchmaker-backend-vjb7.onrender.com/api",
   timeout: 30000,
   headers: { 'Content-Type': 'application/json' },
 });
